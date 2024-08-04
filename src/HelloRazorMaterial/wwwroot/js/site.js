9b18a295f64e842c30ba8231fafdc50a3e5387f3
Christopher Sampson <sammosampson@gmail.com>
2024-08-04 18:42:31 +0100

    initial

---
+++ src/HelloRazorMaterial/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿const buttons = document.querySelectorAll('.mdc-button');
+for (const button of buttons) {
+    mdc.ripple.MDCRipple.attachTo(button);
+}
+
+const textFields = document.querySelectorAll('.mdc-text-field');
+for (const textField of textFields) {
+    mdc.textField.MDCTextField.attachTo(textField);
+}
