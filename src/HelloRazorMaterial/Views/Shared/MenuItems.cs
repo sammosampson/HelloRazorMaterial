e2a900896682117ed120dd6ff9f791cdaaa3bbf8
James Goodenough <james.goodenough>
2024-08-18 16:46:40 +0100

    Refactor

--- src/HelloRazorMaterial/Views/Shared/MenuItems.cs
+++ src/HelloRazorMaterial/Views/Shared/MenuItems.cs
@@ -2,7 +2,7 @@ namespace HelloRazorMaterial.Views.Shared;
 
 public static class MenuItems
 {
-    public static IEnumerable<MenuItem> GetItems(string controllerName) =>
+    public static IEnumerable<MenuItem> Get(string controllerName) =>
         DefaultMenuItems
             .Get(controllerName)
             .Union(CustomMenuItems
