275ee44e66a9cedecaf95c73133a20c5b02db391
Christopher Sampson <sammosampson@gmail.com>
2024-08-29 05:49:48 +0100

    moved logout to account

--- src/HelloRazorMaterial/Controllers/Account/Account.cs
+++ src/HelloRazorMaterial/Controllers/Account/Account.cs
@@ -4,6 +4,8 @@ namespace HelloRazorMaterial.Controllers.Account
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Authorization;
     using HelloRazorMaterial.Controllers.Shared;
+    using HelloRazorMaterial.Abstractions;
+    using Microsoft.AspNetCore.Authentication;
 
     [Authorize]
     public class Account : Controller
@@ -11,6 +13,13 @@ namespace HelloRazorMaterial.Controllers.Account
         public IActionResult Index()
         {
             return View(new MenuModel { ControllerName = nameof(Account) });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(AuthorisationConstants.CookieAuth);
+            return RedirectToAction(nameof(Index), nameof(Home.Home));
         }
     }
 }
