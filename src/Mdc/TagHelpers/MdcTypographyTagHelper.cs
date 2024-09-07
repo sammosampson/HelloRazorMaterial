f4973a1f1a22858979772f1669d6500afc02ec80
Christopher Sampson <sammosampson@gmail.com>
2024-09-07 10:10:00 +0100

    merge

---
+++ src/Mdc/TagHelpers/MdcTypographyTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SystemDot.Web.Razor.Mdc.Generation;
+
+namespace SystemDot.Web.Razor.Mdc.TagHelpers
+{
+    [HtmlTargetElement("mdc-typography")]
+    public class MdcTypographyTagHelper : TagHelper
+    {
+        public MdcTypographyType Type { get; set; }
+        public HtmlTag Tag { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder builder = TypographyGenerator.GenerateTypography(Type);
+            output.TagName = builder.TagName;
+            output.MergeAttributes(builder);
+            output.PostContent.AppendHtml(builder.InnerHtml);
+        }
+    }
+}
